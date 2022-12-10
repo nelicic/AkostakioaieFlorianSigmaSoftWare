e3b913246f41cc025065ec27c76f3093989c71d4
Nelik <112435537+nelicic@users.noreply.github.com>
2022-12-11 00:48:56 +0200

    hw 9

--- HomeWork9/HomeWork9/Program.cs
+++ HomeWork9/HomeWork9/Program.cs
@@ -4,18 +4,10 @@ using System.Globalization;
 
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-/*FileSort fileSort = new FileSort("E:\\C#\\SigmaSoftware\\HomeWork9\\HomeWork9\\");
-fileSort.Sort("data.txt");*/
+FileSort fileSort = new FileSort("E:\\C#\\SigmaSoftware\\HomeWork9\\HomeWork9\\");
+fileSort.Sort("data.txt");
 
-/*QuickSort quickSort = new QuickSort();
-Product[] product = new Product[20];
-
-for (int i = 0; i < product.Length; i++)
-    product[i] = new Product();
-
-product = quickSort.SortArray(product, 0, product.Length - 1, "random");
-
-foreach (Product p in product)
-    Console.WriteLine(p);*/
-
+// quickSort я перевіряв через BenchmarkDotNet
+// прийшлось і в ньому розбиратись
+// (дивіться скрін)
 var result = BenchmarkRunner.Run<BenchMark>(new DebugConfig());
